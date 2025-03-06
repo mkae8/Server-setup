b380e368baf2eeefcaab328574562fbb2682206c
mkae <mkaedeveloper@mkae.local>
2025-03-06 14:47:34 +0800

    fe

---
+++ client/src/shared/types/models/issue.tsx
@@ -0,0 +1,10 @@
+export type TIssue = {
+  volume_id: number
+  title_en: string
+  title_mn: string
+  title_tr: string
+  image_url: string
+  description_en: string
+  description_mn: string
+  description_tr: string
+}
